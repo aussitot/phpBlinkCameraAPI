e7c3526252e081276a5c46b71c0369bb2cd1a55a
Havok <contact@aussitot.net>
2018-11-30 14:03:03 +0100

    Initial commit

---
+++ blink.php
@@ -0,0 +1,10 @@
+<?php
+
+require("phpBlinkCameraAPI.php"); //php class API of the Blink Camera
+$blink_username = "blink email";
+$blink_password = "blink password";
+
+$blink = new BlinkCamera($blink_username, $blink_password);
+$blink->Arm();
+
+?>
